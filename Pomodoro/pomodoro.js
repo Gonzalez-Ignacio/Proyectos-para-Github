e6768cd06aace670d19e76c4aa5df5bbca04404b
Nahuel <131316681+Nahuel-Salvatierra@users.noreply.github.com>
2023-04-26 19:41:35 -0300

    constantes

---
+++ Pomodoro/pomodoro.js
@@ -0,0 +1,6 @@
+// Constante con su id
+const Temporizador = document.getElementById('temporizador');
+const TiempoTrabajo = document.getElementById('tiempoTrabajado');
+const TiempoDescanso = document.getElementById('tiempoDescanso');
+const TiempoDescansoLargo = document.getElementById('descansoLargo');
+
