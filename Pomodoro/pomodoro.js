cacf95d6b1952c34ddbc42a504647a777d0eb2ea
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-04-27 00:05:25 -0300

    Cambio de Texto-Card-input/boton

--- Pomodoro/pomodoro.js
+++ Pomodoro/pomodoro.js
@@ -1,10 +1,12 @@
 // Constante con su id
+const colorCard = document.getElementById("colorCard");
 const Temporizador = document.getElementById('temporizador');
+const Boton = document.getElementById('btn-Iniciar');
 const TiempoTrabajo = document.getElementById('tiempoTrabajado');
 const TiempoDescanso = document.getElementById('tiempoDescanso');
 const TiempoDescansoLargo = document.getElementById('descansoLargo');
 const SeleccionTexto = document.getElementById('cambiar-Texto');
-const Boton = document.getElementById('btn-Iniciar');
+const colorDentro = document.querySelectorAll(".colorDentro");
 
 let intervalo = 1000
 
@@ -14,10 +16,23 @@ Boton.addEventListener('click', function() {
 
       setInterval(
         function(){
+          // Temporizador
           const minutos = Math.floor(tiempoRestante/60)
           const segundos = tiempoRestante % 60
           Temporizador.innerText=`${minutos}:${segundos}`
           tiempoRestante--;
+
+          // Cambio de texto
+          SeleccionTexto.innerText = "Tiempo de trabajo iniciado"
+          // Cambio de Color de Carta
+          colorCard.style.backgroundColor = "green";
+          // Cambio de color de input y boton
+          colorDentro.forEach(elemento => {
+            elemento.style.backgroundColor = "blue"
+          });
+          
+
+         
         },
         intervalo
         )
