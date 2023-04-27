8c4744e599ef86c207666868ee8bab70cdf7356f
Nahuel <131316681+Nahuel-Salvatierra@users.noreply.github.com>
2023-04-27 00:28:13 -0300

    ajuste en el contador
    ajuste en el contador principal para que no cuente tiempo negativo

--- Pomodoro/pomodoro.js
+++ Pomodoro/pomodoro.js
@@ -8,31 +8,36 @@ const TiempoDescansoLargo = document.getElementById('descansoLargo');
 const SeleccionTexto = document.getElementById('cambiar-Texto');
 const colorDentro = document.querySelectorAll(".colorDentro");
 
-let intervalo = 1000
+let intervalo = 100
 
 Boton.addEventListener('click', function() {
-      let tiempoTotal = document.getElementById('tiempoTrabajado').value * 60 + document.getElementById('tiempoTrabajado').value * 60;
+      let tiempoTotal = document.getElementById('tiempoTrabajado').value * 60
+      + document.getElementById('tiempoDescanso').value * 60 
+      + document.getElementById('descansoLargo').value *60;
       let tiempoRestante = tiempoTotal
 
-      setInterval(
+      // Cambio de texto
+      SeleccionTexto.innerText = "Tiempo de trabajo iniciado"
+      // Cambio de Color de Carta
+      colorCard.style.backgroundColor = "green";
+      // Cambio de color de input y boton
+      colorDentro.forEach(elemento => {
+        elemento.style.backgroundColor = "blue"
+      });
+
+      const contador = setInterval(
         function(){
           // Temporizador
           const minutos = Math.floor(tiempoRestante/60)
           const segundos = tiempoRestante % 60
-          Temporizador.innerText=`${minutos}:${segundos}`
+          Temporizador.innerText=`${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
           tiempoRestante--;
+          if (tiempoRestante<0)
+            {
+              clearInterval(contador)
+              SeleccionTexto.innerText = "Tiempo Terminado! A descansar"
+            }
 
-          // Cambio de texto
-          SeleccionTexto.innerText = "Tiempo de trabajo iniciado"
-          // Cambio de Color de Carta
-          colorCard.style.backgroundColor = "green";
-          // Cambio de color de input y boton
-          colorDentro.forEach(elemento => {
-            elemento.style.backgroundColor = "blue"
-          });
-          
-
-         
         },
         intervalo
         )
