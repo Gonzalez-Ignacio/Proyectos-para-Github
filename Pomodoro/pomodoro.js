1c4d9a8ef4016867438ea02d2afe45e3bd62283f
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-04-27 19:01:06 -0300

    Funcionalidad boton Pausa y Continuar

--- Pomodoro/pomodoro.js
+++ Pomodoro/pomodoro.js
@@ -7,6 +7,8 @@ const TiempoDescanso = document.getElementById('tiempoDescanso');
 const TiempoDescansoLargo = document.getElementById('descansoLargo');
 const SeleccionTexto = document.getElementById('cambiar-Texto');
 const colorDentro = document.querySelectorAll(".colorDentro");
+const btnPausar = document.getElementById("btn-Pausar");
+const btnReiniciar = document.getElementById("btn-Reiniciar");
 
 let intervalo = 100
 
@@ -15,6 +17,10 @@ Boton.addEventListener('click', function() {
       + document.getElementById('tiempoDescanso').value * 60 
       + document.getElementById('descansoLargo').value *60;
       let tiempoRestante = tiempoTotal
+      btnPausar.disabled = false
+      btnReiniciar.disabled = false
+      Boton.disabled = true
+      
 
       // Cambio de texto
       SeleccionTexto.innerText = "Tiempo de trabajo iniciado"
@@ -25,22 +31,71 @@ Boton.addEventListener('click', function() {
         elemento.style.backgroundColor = "blue"
       });
 
-      const contador = setInterval(
-        function(){
-          // Temporizador
-          const minutos = Math.floor(tiempoRestante/60)
-          const segundos = tiempoRestante % 60
-          Temporizador.innerText=`${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
-          tiempoRestante--;
-          if (tiempoRestante<0)
-            {
-              clearInterval(contador)
-              SeleccionTexto.innerText = "Tiempo Terminado! A descansar"
-            }
+        let contador;
+        function iniciarTemporizador() {
+          contador = setInterval(function() {
+            // Temporizador
+            const minutos = Math.floor(tiempoRestante/60)
+            const segundos = tiempoRestante % 60
+            Temporizador.innerText=`${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
+            tiempoRestante--;
+            if (tiempoRestante<0)
+              {
+                clearInterval(contador)
+                SeleccionTexto.innerText = "Tiempo Terminado! A descansar"
+              }
+          },
+          intervalo)
+        }
+  
+        iniciarTemporizador(); 
+  
+        let count = 0;
+        btnPausar.addEventListener("click", () => {
+          if(count == 0) {
+            count = 1;
+            clearInterval(contador);
+            btnPausar.innerHTML = '<i class="bi bi-skip-start-circle"></i>';
+          } else {
+            count = 0;
+            iniciarTemporizador();
+            btnPausar.innerHTML = '<i class="bi bi-pause-circle-fill"> </i>';
+          }
+        })
+  })
 
-        },
-        intervalo
-        )
-  }
-)
 
+
+
+  // let contador;
+  // let count = 0;
+  
+  // function iniciarTemporizador() {
+  //   contador = setInterval(function() {
+  //     // Temporizador
+  //     const minutos = Math.floor(tiempoRestante/60);
+  //     const segundos = tiempoRestante % 60;
+  //     Temporizador.innerText =`${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
+  //     tiempoRestante--;
+  
+  //     if (tiempoRestante < 0) {
+  //       clearInterval(contador);
+  //       SeleccionTexto.innerText = "Tiempo Terminado! A descansar";
+  //     }
+  //   }, intervalo);
+  // }
+  
+  // iniciarTemporizador();
+  
+  // btnPausar.addEventListener("click", () => {
+  //   if (count === 0) {
+  //     count = 1;
+  //     clearInterval(contador);
+  //     btnPausar.innerHTML = 'Continuar <i class="bi bi-skip-start-circle"></i>';
+  //   } else {
+  //     count = 0;
+  //     iniciarTemporizador();
+  //     btnPausar.innerHTML = 'Pausa <i class="bi bi-pause-circle-fill"></i>';
+  //   }
+  // });
+  
