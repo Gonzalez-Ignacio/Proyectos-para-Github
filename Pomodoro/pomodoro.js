b08e3b59f2465305658c8f98df58a8cb272ba472
Nahuel <131316681+Nahuel-Salvatierra@users.noreply.github.com>
2023-04-26 21:14:31 -0300

    se creo el js que funciona
    se creo el bucle en el cual el internvalo de tiempo empieza a descontar pero faltan hacer ajustes

--- Pomodoro/pomodoro.js
+++ Pomodoro/pomodoro.js
@@ -3,4 +3,22 @@ const Temporizador = document.getElementById('temporizador');
 const TiempoTrabajo = document.getElementById('tiempoTrabajado');
 const TiempoDescanso = document.getElementById('tiempoDescanso');
 const TiempoDescansoLargo = document.getElementById('descansoLargo');
+const SeleccionTexto = document.getElementById('cambiar-Texto');
+const Boton = document.getElementById('btn-Iniciar');
+
+let intervalo = 1000
+
+Boton.addEventListener('click', function() {
+      let asdf = document.getElementById('tiempoTrabajado').value;
+      console.log(asdf)
+      setInterval(
+        function(){
+          Temporizador.innerText=asdf;
+          asdf--;
+
+        },
+        intervalo
+        )
+  }
+)
 
